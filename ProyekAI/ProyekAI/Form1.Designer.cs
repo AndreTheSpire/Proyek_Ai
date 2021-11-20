848fa1b9516e463b3f2ccdddb78e75865b98d23d
AndreTheSpire <64310095+AndreTheSpire@users.noreply.github.com>
2021-11-20 14:34:58 +0700

    setting awal

--- ProyekAI/ProyekAI/Form1.Designer.cs
+++ ProyekAI/ProyekAI/Form1.Designer.cs
@@ -29,45 +29,61 @@ namespace ProyekAI
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(132, 146);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
-            this.label1.TabIndex = 0;
+            this.panel1.Location = new System.Drawing.Point(24, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
             // 
-            // label2
+            // panel2
             // 
-            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.label2.Location = new System.Drawing.Point(109, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 60);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.panel2.Location = new System.Drawing.Point(439, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(24, 434);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 400);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(439, 434);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(400, 400);
+            this.panel4.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1237, 671);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1293, 892);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
--- ProyekAI/ProyekAI/Form1.cs
+++ ProyekAI/ProyekAI/Form1.cs
@@ -15,11 +15,42 @@ namespace ProyekAI
         public Form1()
         {
             InitializeComponent();
+            createLabel(panel1);
+            createLabel(panel2);
+            createLabel(panel3);
+            createLabel(panel4);
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+        public void createLabel(Panel panelx)
+        {
+            int x = 0, y = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Label l = new Label();
+                    l.Size = new Size(100, 100);
+
+                    l.Location = new Point(x, y);
+                    if (j % 2 == 0)
+                    {
+                        l.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        l.BackColor = Color.Blue;
+                    }
+
+                    panelx.Controls.Add(l);
+                    x += 100;
+                }
+                x = 0;
+                y += 100;
+            }
         }
     }
 }
